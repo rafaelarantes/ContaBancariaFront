4b7d980a7d16f328dea1a1b9f5eafe7b7cebd69e
Rafael Arantes <rafaeltwi@hotmail.com>
2024-06-04 16:18:18 -0300

    Added search and Add button layout in banco-central

--- ContaBancaria/src/app/banco-central/components/listar-banco-central/listar-banco-central.component.ts
+++ ContaBancaria/src/app/banco-central/components/listar-banco-central/listar-banco-central.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 import { TableComponent } from '../../../shared/components/table/component/table.component';
 import { BancoCentralService } from '../../services/banco-central.service';
@@ -9,27 +11,26 @@ import { BaseComponent } from '../../../shared/components/base/base.component';
 @Component({
   selector: 'app-listar-banco-central',
   standalone: true,
-  imports: [ TableComponent],
+  imports: [ TableComponent, MatButtonModule, MatIconModule, MatInputModule ],
   templateUrl: './listar-banco-central.component.html',
   styleUrl: './listar-banco-central.component.scss',
 })
 export class ListarBancoCentralComponent extends BaseComponent implements AfterViewInit {
   @ViewChild(TableComponent) tableComponent: TableComponent = <TableComponent>{};
-  returnUrl: string = '/';
 
-  constructor(private route: ActivatedRoute,
-              private bancoCentralService: BancoCentralService,
+  constructor(private bancoCentralService: BancoCentralService,
               private tituloService: TituloService) {
     super();
-    this.returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     tituloService.setTitulo('Banco Central');
   }
 
   async ngAfterViewInit() {
     let columns = new Map<string, string>();
+
     columns.set('nome', 'Nome');
     columns.set('agencia', 'Agência');
     columns.set('numero', 'Número');
+    
     this.tableComponent.setColumns = columns;
 
     let returno = this.bancoCentralService.listagem();
